4b836d9856d3f2b687c89f7f958ffda1a8a49b12
Alyssa L <alyssal282006@gmail.com>
2026-07-17 12:17:08 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        //being silly brute force will try again aiyooooooo
+        double ans;
+        return pow(x,n);
+        
+    }
+};
