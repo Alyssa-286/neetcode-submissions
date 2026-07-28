478eeb1d09ebbbe117a0fcbfcc6d4d685306411b
Alyssa L <alyssal282006@gmail.com>
2026-07-28 15:12:05 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,vector<int>>>maxheap;//why cant i use priority_queue<pair<int,pair<int,int>>>maxheap
+        for(auto p:points)
+        {
+            int x=p[0];
+            int y=p[1];
+            int dist=x*x+y*y;
+            maxheap.push({dist,p});
+            if(maxheap.size()>k)
+            maxheap.pop();
+        }
+        vector<vector<int>>result;
+        while(!maxheap.empty())
+        {
+            result.push_back(maxheap.top().second);
+            maxheap.pop();
+        }
+        return result;
+    }
+};
